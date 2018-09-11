c91cadc59e09bd620d7355f13a9884e01a31c0fa
ryoi084 <ryoi084@outlook.jp>
2018-09-11 23:12:13 +0900

    practiceを追加

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Neural_Network
+
+# practice 
+[このサイト](https://nnadl-ja.github.io/nnadl_site_ja/chap1.html)の第一章のコードを, python3.xでも動くように編集しました.
+
+## 実行方法
+```
+				python run.py
+```
+とコマンドを打ってください.
+
+そうすると, ニューラルのプログラムが動き出すかと思います.
---
+++ practice/run.py
@@ -0,0 +1,27 @@
+import mnist_loader
+
+import network
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+input_size = 28 * 28
+hidden_size = 10
+hidden_layer_num = 2
+output_size = 10
+
+Neural_structure = [input_size]
+for l in range(hidden_layer_num):
+    Neural_structure.append(hidden_size)
+Neural_structure.append(output_size)
+
+net = network.Network(Neural_structure)
+
+print('Layer # = %d'%(hidden_layer_num))
+print('NN structure:',Neural_structure)
+
+
+epochs = 10
+mini_batch_size = 10
+eta = 3.0
+
+net.SGD(training_data, epochs, mini_batch_size, eta, test_data=test_data)
