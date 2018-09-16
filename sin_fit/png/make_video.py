fd04ac5c0d6fe1ac8eaeef7562fd511dd2a623b9
ryoi084 <ryoi084@outlook.jp>
2018-09-16 14:27:23 +0900

    sin_fitで動画を作成

---
+++ sin_fit/png/make_video.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('ffmpeg -r 2 -i result_epoch=%03d.png -vcodec libx264 -pix_fmt yuv420p -r 4 out.mp4')
--- sin_fit/run.py
+++ sin_fit/run.py
@@ -1,8 +1,9 @@
 import Network
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-N = 100000
+N = 50000
 
 X = np.random.rand(N)*2.0*np.pi
 Y = np.sin(X)
@@ -26,25 +27,36 @@ print('Layer # = %d'%(hidden_layer_num))
 print('NN structure:',Neural_structure)
 
 
-epochs = 10
+epochs_num = 50
 mini_batch_size = 10
 eta = 0.1
 
-net.SGD(training_data, epochs, mini_batch_size, eta, 
-        lmbda = 5.0,
-        )
+ims = []
 
-x = []
-sin = []
-for z in np.linspace(0.0, 2.0*np.pi, 100):
-    x.append(z)
-    sin.append((net.feedforward(np.array([[z]]))[0]))
-    
-Y = Y[np.argsort(X)]
-X = np.sort(X)
-plt.title("Epoch = %d" %epochs)
-plt.plot(X, Y, label='training_data', linestyle='dashed')
-plt.plot(x, sin, label='Neural_Network')
-plt.legend()
-plt.savefig('result_epoch=%d.png'%epochs)
-# plt.show()
+Y_plot = Y[np.argsort(X)]
+X_plot = np.sort(X)
+
+for epochs in range(50):
+    '''
+    net.SGD(training_data, epochs, mini_batch_size, eta, 
+            lmbda = 5.0,
+            )
+    '''
+    net.SGD(training_data, 1, mini_batch_size, eta, 
+            lmbda = 5.0,
+            )
+
+    x = []
+    sin = []
+    for z in np.linspace(0.0, 2.0*np.pi, 100):
+        x.append(z)
+        sin.append((net.feedforward(np.array([[z]]))[0]))
+        
+    plt.title("Epoch = %d" %epochs)
+    plt.plot(X_plot, Y_plot, label='training_data', linestyle='dashed')
+    plt.plot(x, sin, label='Neural_Network')
+    plt.savefig('./png/result_epoch=%03d.png'%epochs)
+    plt.close()
+    # plt.show()
+
+
