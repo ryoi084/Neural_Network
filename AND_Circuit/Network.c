567ac236275c053d71e4baec2dcfdb6efc06dd00
ryoi084 <ryoi084@outlook.jp>
2018-09-24 21:44:16 +0900

    forward関数を実装

--- AND_Circuit/Network.c
+++ AND_Circuit/Network.c
@@ -11,8 +11,9 @@
 void sigmoid(int N, double X[N], double Z[N]);
 void deriv_sigmoid(int N, double X[N], double Z[N]);
 
+void forward(int N1, int N2, double W[N1][N2], double b[N1], double X[N2], double Y[N1]);
 double train(int N1, int N2, double W[N1][N2], double b[N1], double X[N2], double T[N1], double learning_rate);
-double test(int N1, int N2, double W[N1][N2], double b[N1], double X[N2], double T[N1], double Pred_Y[N1]);
+double test(int N1, int N2, double W[N1][N2], double b[N1], double X[N2], double T[N1], double Y[N1]);
 
 void MV(int N1, int N2, double M[N1][N2], double X[N2], double Y[N1]);
 void input_V(int N, double X[N], double Y[N]);
@@ -42,7 +43,7 @@ void main(void){
 
 
 				// 学習開始
-				for(int epoch=1; epoch<MAX_EPOCH; epoch++){
+				for(int epoch=1; epoch<MAX_EPOCH+1; epoch++){
 								for(int l=0; l<4; l++){
 												cost = train(1,2,W,b,train_X[l], train_Y[l],lr);
 								}
@@ -85,18 +86,26 @@ void deriv_sigmoid(int N, double X[N], double Z[N]){
 
 // -------------------------------------------------------------------------------
 // ニューラルネットワークの関数定義
+// コスト関数: クロスエントロピー
+// 最適化法: 確立勾配法
 // -------------------------------------------------------------------------------
+
+void forward(int N1, int N2, double W[N1][N2], double b[N1], double X[N2], double Y[N1]){
+				double buf[N1];
+
+				MV(N1,N2,W,X,buf);
+				for(int i=0; i<N1; i++){buf[i] += b[i];}
+				sigmoid(N1,buf,Y);
+}
+
 double train(int N1, int N2, double W[N1][N2], double b[N1], double X[N2], double T[N1], double learning_rate){
 				double Y[N1];
-				double buf[N1];
 				double delta[N1];
 				double cost=0;
 
 
 				// Forward Propagation
-				MV(N1,N2,W,X,buf);
-				for(int i=0; i<N1; i++){buf[i] += b[i];}
-				sigmoid(N1,buf,Y);
+				forward(N1,N2,W,b,X,Y);
 
 				// Back Propagation 
 				for(int i=0; i<N1; i++){
@@ -117,19 +126,14 @@ double train(int N1, int N2, double W[N1][N2], double b[N1], double X[N2], doubl
 }
 
 
-double test(int N1, int N2, double W[N1][N2], double b[N1], double X[N2], double T[N1], double Pred_Y[N1]){
-				double Y[N1];
-				double buf[N1];
+double test(int N1, int N2, double W[N1][N2], double b[N1], double X[N2], double T[N1], double Y[N1]){
 				double cost=0;
 
-				// Test Cost
-				MV(N1,N2,W,X,buf);
-				for(int i=0; i<N1; i++){buf[i] += b[i];}
-				sigmoid(N1,buf,Y);
+				// Forward Propagation
+				forward(N1,N2,W,b,X,Y);
 
 				for(int i=0; i<N1; i++){
 								cost += -T[i]*log(Y[i]) - (1.0-T[i])*log(1.0-Y[i]);
-								Pred_Y[i] = Y[i];
 				}
 				return cost;
 }
